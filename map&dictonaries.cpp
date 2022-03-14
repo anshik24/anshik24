ad54db89dea9fdc3f070ba742b40f431cbfd2faa
Anshika Singh <99993107+anshik24@users.noreply.github.com>
2022-03-14 16:31:02 +0530

    Create map&dictonaries.cpp

---
+++ map&dictonaries.cpp
@@ -0,0 +1,34 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <map>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    std::map<string,string>phoneBook;
+    int n;cin>>n;
+    for(int i=0;i<n;i++){
+        string name;
+        string phone;
+        cin>>name;
+        cin>>phone;
+        phoneBook[name]=phone;
+    }
+    std::map<string,string>::iterator it;
+    string query;
+    while(cin>>query){
+        it =phoneBook.find(query);
+        
+        if(it!=phoneBook.end()){
+            cout<<it->first<<"="<<it->second<<'\n';
+        }
+        else{
+            cout<<"Not found"<<'\n';
+        }
+    }
+    return 0;
+}
